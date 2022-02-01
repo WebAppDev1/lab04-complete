e39c774e5883aa967ac1fc41eb72e385b4a9a57f
Glitch (lab04-webapp-2022) <none>
2022-02-01 12:04:01 +0000

    🩳😃 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -1,9 +1,7 @@
-/* If you're feeling fancy you can add interactivity 
+/* If you're feeling fancy you can add interactivity
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
 console.log("Hello from the Web App Dev 1 lab!");
-
 
 const greenbtn = document.querySelector(".green");
 
@@ -39,20 +37,22 @@ redbtn &&
 welcomeUserDiv &&
   welcomeUserDiv.addEventListener("click", (evt) => {
    // evt.currentTarget.style.display = "none";
+    welcomeUserDiv.style.display = "none";
+  });
 
+const ratebtn  = document.querySelector("#rateit");
 
-const yellowbtn  = document.querySelector(".yellow");
-
-yellowbtn &&
-  yellowbtn.addEventListener("click", () => {
+ratebtn &&
+  ratebtn.addEventListener("click", () => {
    let userRating = parseInt(prompt("Rate this collection (from 1 to 5 stars)"));
   if (userRating>5 || userRating<1 || isNaN(userRating)){
     alert("Try again with a number between 1 and 5!");
   }
   else{
-    $("#rating").html("You gave a rating of: ");
+
+    document.querySelector("#rating").innerHTML = "You gave a rating of: ";
     for (let i=0; i < userRating; i++){
-        $("#rating").append("<i class='yellow star icon'></i>");
+        document.querySelector("#rating").innerHTML +="<i class='yellow star icon'></i>";
     }
   }
 });
